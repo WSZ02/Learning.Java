0ce48bd4e89c92c6d0d5afe5622111091155e0da
WSZ02 <w.szymanek02@gmail.com>
2023-07-30 11:52:46 +0200

    Ukończenie LoginSystem

--- LoginSystem/src/IDandPasswords.java
+++ LoginSystem/src/IDandPasswords.java
@@ -1,2 +1,17 @@
+import java.util.HashMap;
+
 public class IDandPasswords {
+
+    HashMap<String, String> logininfo = new HashMap<String, String>();
+
+    IDandPasswords(){
+
+        logininfo.put("Michal","Rama21");
+        logininfo.put("Czarodziej12","PASSWORD02");
+        logininfo.put("Wartownik","Test01");
+
+    }
+    protected HashMap getLoginInfo(){
+        return logininfo;
+    }
 }
