c0f0a9b01be4f3e22ad69d983ead9529a7c9ef0d
Wojtek <w.szymanek02@gmail.com>
2023-08-01 14:26:22 +0200

    Zrobiona 1 część 'CurrencyConvertera'

--- CurrencyConverter/src/CurrencyConverter.java
+++ CurrencyConverter/src/CurrencyConverter.java
@@ -1,9 +1,66 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Currency;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class CurrencyConverter {
 
 
     public static void main(String[] args) {
 
-        
+        HashMap<Integer, String> currencyCodes = new HashMap<Integer, String>();
 
+        currencyCodes.put(1, "USD");
+        currencyCodes.put(2, "CAD");
+        currencyCodes.put(3, "EUR");
+        currencyCodes.put(4, "HKD");
+        currencyCodes.put(5, "INR");
+
+        String fromCode, toCode;
+        double amount;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the currency converter!");
+
+        System.out.println("Currency converting FROM?");
+        System.out.println("1:USD (US dollar)\t 2:CAD (Canadian Dollar)\t 3:EUR (Euro) \t 4:HKD (Hong Kong Dollar) \t 5:INR (Indian Rupee");
+        fromCode = currencyCodes.get(sc.nextInt());
+
+        System.out.println("Currency converting TO?");
+        System.out.println("1:USD (US dollar)\t 2:CAD (Canadian Dollar)\t 3:EUR (Euro) \t 4:HKD (Hong Kong Dollar) \t 5:INR (Indian Rupee");
+
+        System.out.println("Amount you wish to convert?");
+        amount = sc.nextFloat();
+
+        //sendHttpGETRequest(fromCode, toCode, amount);
+
+        System.out.println("Thank you for using the currency converter!");
+
+    }
+
+    private static void sendHttpGETRequest(String fromCode, String toCode, double amount) throws IOException {
+
+        String GET_URL = "http://api.exchangeratesapi.io/v1/latest?" + toCode + "&symbols=" + fromCode;
+        URL url = new URL(GET_URL);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("GET");
+        int responseCode = httpURLConnection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while((inputLine = in.readLine()) != null){
+                response.append(inputLine);
+            }in.close();
+
+        }
     }
 }
